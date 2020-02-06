52085db17734b48a4bbd3ca3ba2f8056270c3a24
Shaung Cheng <shaungc@umich.edu>
2020-02-05 23:52:43 -0800

    add k8 provider; fix k8 error

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -22,3 +22,21 @@ resource "local_file" "kubeconfig" {
     # filename = "${path.module}/kubeconfig.yaml"
     filename = "kubeconfig.yaml"
 }
+
+
+provider "kubernetes" {
+  # all k8 provider versions: https://github.com/terraform-providers/terraform-provider-kubernetes/blob/master/CHANGELOG.md
+  version = "~> 1.10"
+
+  host = data.digitalocean_kubernetes_cluster.for_app.endpoint
+
+  token = data.digitalocean_kubernetes_cluster.for_app.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.for_app.kube_config[0].cluster_ca_certificate
+  )
+  
+  # adding this block to resolve tf error: `<a k8 resource> is forbidden: User "system:anonymous cannot create resource "<a k8 resource>" in API group "" at the cluster scope`
+  client_certificate     = data.digitalocean_kubernetes_cluster.for_app.kube_config[0].client_certificate
+  client_key             = data.digitalocean_kubernetes_cluster.for_app.kube_config[0].client_key
+  
+}
