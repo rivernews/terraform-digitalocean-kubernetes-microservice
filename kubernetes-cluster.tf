b9dbd72ca0f11bb4503946b69cb19978e97c05b9
Shaung Cheng <shaung.cheng@light.co>
2021-07-05 23:20:07 -0700

    update built-in provider version

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -6,10 +6,6 @@ data "digitalocean_kubernetes_cluster" "for_app" {
     name = var.cluster_name
 }
 
-provider "local" {
-    version = "~> 1.3"
-}
-
 resource "local_file" "kubeconfig" {
     sensitive_content     = data.digitalocean_kubernetes_cluster.for_app.kube_config.0.raw_config
     # filename = "${path.module}/kubeconfig.yaml"
