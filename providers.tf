3f43e423e3bdfb340e79deab59fa7c45120c8569
Shaung Cheng <shaung.cheng@light.co>
2021-07-05 23:01:56 -0700

    rm deprecated version format

--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
 provider "digitalocean" {
   token   = data.aws_ssm_parameter.digitalocean_token.value
 }
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
